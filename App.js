5658d840ce427834ec5202237d47ce4d8900f616
Karthik R <karthikr.ofc@gmail.com>
2022-06-26 00:01:55 +0530

    Changes

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { StyleSheet,Text,View } from "react-native";
+import Player from './src/musicPlayer';
 import Icon from 'react-native-vector-icons/MaterialIcons'//import the icons.Specified in build.gradle
+
 
 export default function App(){
   return(
-    <View style = {{flex: 1, alignItems: "center",justifyContent:"space-around"}}>
-      <Text style={{fontSize:40,textAlign:"center"}}>Music Player</Text>
-      <Icon name="music" size={200}/>
+    <View style={styles.container}>
+      <Player/>
     </View>
-  )
+    // <View style = {{flex: 1, alignItems: "center",justifyContent:"space-around"}}>
+    //   <Text style={{fontSize:40,textAlign:"center"}}>Music Player</Text>
+    //   <Icon name="music-note" size={200}/>
+    // </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "fff",
+    justifyContent:'center'
+  }
+})
---
+++ src/musicPlayer.js
@@ -0,0 +1,3 @@
+import React from "react";
+import {View,SafeAreaView,Text,Image,FlatList,Dimensions,Animated,StyleSheet,} from "react-native";
+
